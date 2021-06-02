d6f25a871173817a7a7258f92304a90e71f0c662
Reididsorg <bruno.grosdidier@gmail.com>
2021-06-02 14:59:07 +0200

    Added more comments functionnality

--- public/js/displayTrick.js
+++ public/js/displayTrick.js
@@ -1,6 +1,39 @@
 jQuery(document).ready(function() {
     $('#seeMedias').click(function(e) {
-        console.warn('hello');
         $('#medias').show();
     });
+
+    /**
+     * Simple (ugly) code to load more comments
+     */
+    var click = 0;
+    $('#loadMoreComments').on('click', function(e) {
+        click++;
+        var offset = 5 * click;
+        var limit = 0;
+        /*console.warn(offset);*/
+        /* Javascript data attribute */
+        var comments = e.target.dataset.commentstodisplay;
+        var commentsJsonObj = JSON.parse(comments);
+        $.each(commentsJsonObj, function( index, value ) {
+            if (index >= offset) {
+                var commentToDisplay = '<div class="comment row mx-auto">\n' +
+                    '    <div class="col-xl-8 col-lg-10 col-12 row mx-auto">\n' +
+                    '        <p class="col-lg-2 col-12 d-none d-lg-block  comment-img">\n' +
+                    '            <img src="'+value.userImagePath+value.userImageFilename+'" class="rounded mx-auto d-block" alt="'+value.userImageAlt+'" height="50">\n' +
+                    '        </p>\n' +
+                    '        <p class="col-lg-10 col-12 comment-text">\n' +
+                    '            <strong>'+value.userName+'</strong> <small class="text-muted">('+value.trickCreatedAt+') :</small>\n' +
+                    '            <br>'+value.content+'\n' +
+                    '        </p>\n' +
+                    '    </div>\n' +
+                    '</div>';
+                    if (limit < 5) {
+                        $('#comments').append(commentToDisplay);
+                    }
+                limit++;
+            }
+        });
+    });
+
 });
